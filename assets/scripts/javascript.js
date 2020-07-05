eaac92e0d41738a45cc3ae1ad00015fb32562f61
DANIEL <daniel_cornwall@hotmail.com>
2020-07-05 11:47:35 +0000

    end play screen good right card only on event

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -29,6 +29,9 @@ function hilo(a){
     var cardclear=document.getElementById('cardtodelete');
     cardclear.remove();
 
+    var cardclear=document.getElementById('cardtodeleteright');
+    cardclear.remove();
+    
     if(a == 'high' && oldCard < newCard){win=true;}
     else if(a == 'low' && oldCard > newCard){win=true;}
     if(win){
@@ -81,7 +84,7 @@ function showCard(){
 }
 //dealing the cards - card on the right
 function showCardeventonly(){
-    return '<div><p>'
+    return '<div id="cardtodeleteright"><p>'
     +cards[count].event+'</p>'
     }
 
@@ -102,10 +105,34 @@ function Card(date, event, description, link, linkDisplay, image){
 
 //deck of cards
 var cards = [
-    new Card(1666,'The Great Fire of London', 'The Great Fire of London swept through the central parts of the English city from Sunday, 2 September to Thursday, 6 September 1666. The fire gutted the medieval City of London inside the old Roman city wall.','https://en.wikipedia.org/wiki/Great_Fire_of_London', 'click for more info', 'greatfirelondon' ),
-    new Card(1820,'When was Florence Nightingale born', 'Florence Nightingale, byname Lady with the Lamp, (born May 12, 1820, Florence [Italy]—died August 13, 1910, London, England), British nurse, statistician, and social reformer who was the foundational philosopher of modern nursing.','https://www.britannica.com/biography/Florence-Nightingale','click for more info', 'nightengale' ),
-    new Card(1891,'When was First Crufts dog Show', 'The first show named Crufts - Crufts Greatest Dog Show — was held at the Royal Agricultural Hall, Islington, in 1891. It was the first at which all breeds were invited to compete, with around 2,000 dogs and almost 2,500 entries.', 'https://en.wikipedia.org/wiki/Crufts', 'click for more info', 'crufts'),
-    new Card(1859,'Charles Darwin’s theory of Evolution is published',  'On the Origin of Species (or, more completely, On the Origin of Species by Means of Natural Selection, or the Preservation of Favoured Races in the Struggle for Life), published on 24 November 1859, is a work of scientific literature by Charles Darwin which is considered to be the foundation of evolutionary biology.','https://en.wikipedia.org/wiki/On_the_Origin_of_Species', 'click for more info', 'darwin'),
+    new Card(
+        1666,
+        'The Great Fire of London', 
+        'The Great Fire of London swept through the central parts of the English city from Sunday, 2 September to Thursday, 6 September 1666. The fire gutted the medieval City of London inside the old Roman city wall.',
+        'https://en.wikipedia.org/wiki/Great_Fire_of_London', 
+        'click for more info', 
+        'greatfirelondon' ),
+    new Card(
+        1820,
+        'When was Florence Nightingale born', 
+        'Florence Nightingale, byname Lady with the Lamp, (born May 12, 1820, Florence [Italy]—died August 13, 1910, London, England), British nurse, statistician, and social reformer who was the foundational philosopher of modern nursing.',
+        'https://www.britannica.com/biography/Florence-Nightingale',
+        'click for more info', 
+        'nightengale' ),
+    new Card(
+        1891,
+        'When was First Crufts dog Show', 
+        'The first show named Crufts - Crufts Greatest Dog Show — was held at the Royal Agricultural Hall, Islington, in 1891. It was the first at which all breeds were invited to compete, with around 2,000 dogs and almost 2,500 entries.',
+        'https://en.wikipedia.org/wiki/Crufts', 
+        'click for more info',
+        'crufts'),
+    new Card(
+        1859,
+        'Charles Darwin’s theory of Evolution is published',  
+        'On the Origin of Species (or, more completely, On the Origin of Species by Means of Natural Selection, or the Preservation of Favoured Races in the Struggle for Life), published on 24 November 1859, is a work of scientific literature by Charles Darwin which is considered to be the foundation of evolutionary biology.',
+        'https://en.wikipedia.org/wiki/On_the_Origin_of_Species', 
+        'click for more info', 
+        'darwin'),
     new Card(1863,'The London underground Open',  'The London Underground (also known simply as the Underground, or by its nickname the Tube) is a public rapid transit system serving London region, England and some parts of the adjacent counties of Buckinghamshire, Essex and Hertfordshire in the United Kingdom.', 'https://en.wikipedia.org/wiki/London_Underground', 'click for more info', 'londonunderground'),
     new Card(1911,'The Launch of the titanic',  'The Titanic was launched into Belfast Lough on May 31, 1911. Ten months later, it struck an iceberg and sank on its maiden voyage to New York City. The unsinkable British liner Titanic sails out of Southampton, England, at the start of its doomed voyage in 1912.', 'https://en.wikipedia.org/wiki/RMS_Titanic', 'click for more info', 'titanic'),
     new Card(1919,'First female MP to sit in the UK parliament', 'The first women to take her seat was Nancy Astor (Viscountess Astor), after a by-election in December 1919. She was elected as a Conservative for the Plymouth Sutton constituency after her husband, Waldorf Astor, the former MP, was elevated to the peerage.', 'https://www.parliament.uk/about/living-heritage/transformingsociety/electionsvoting/womenvote/overview/womenincommons/','click for more info', 'femaleMP'),
