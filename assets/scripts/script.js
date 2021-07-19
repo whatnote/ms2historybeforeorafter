d1a618f99571d61c694a61bccad7c4c561c06517
Daniel Culley <daniel_cornwall@hotmail.com>
2021-07-19 23:57:22 +0100

    js for add card added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -452,3 +452,21 @@ var cards = [
 ];
 
 
+//add card contact data
+
+function sendMail(contactForm) {
+    emailjs.send("default_service", "danielcv", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
